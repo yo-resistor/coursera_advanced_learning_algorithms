133e87c9fbf3d7dab46670f816f501f3d124f885
Yunsik Ohm <yunsikohm@gmail.com>
2024-04-25 10:26:02 -0700

    restart study with week 1 lab 1 file related to tensorflow implementation

---
+++ week_1/w1_lab1.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras import Sequential
+from tensorflow.keras.losses import MeanSquaredError, BinaryCrossentropy
+from tensorflow.keras.activations import sigmoid
+from lab_utils_common import dlc
+from lab_neurons_utils import plt_prob_1d, sigmoidnp, plt_linear, plt_logistic
+plt.style.use('./deeplearning.mplstyle')
+import logging
+logging.getLogger("tensorflow").setLevel(logging.ERROR)
+tf.autograph.set_verbosity(0)
+
+X_train = np.array([[1.0], [2.0]], dtype=np.float32)           #(size in 1000 square feet)
+Y_train = np.array([[300.0], [500.0]], dtype=np.float32)       #(price in 1000s of dollars)
+
+fig, ax = plt.subplots(1,1)
+ax.scatter(X_train, Y_train, marker='x', c='r', label="Data Points")
+ax.legend( fontsize='xx-large')
+ax.set_ylabel('Price (in 1000s of dollars)', fontsize='xx-large')
+ax.set_xlabel('Size (1000 sqft)', fontsize='xx-large')
+plt.show()
